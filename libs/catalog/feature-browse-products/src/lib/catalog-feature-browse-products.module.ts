1ba7a8c66d84b8759d6e0b99aab0524e2e4a6266
Manfred Steyer <manfred.steyer@gmx.net>
2019-05-12 20:40:45 +0100

    ok

--- libs/catalog/feature-browse-products/src/lib/catalog-feature-browse-products.module.ts
+++ libs/catalog/feature-browse-products/src/lib/catalog-feature-browse-products.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
 import { CatalogDataAccessModule } from '@e-proc/catalog/data-access';
 
 @NgModule({
