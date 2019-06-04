565ef784de0ca0a9cace2eb9151354d9ccae4d24
Manfred Steyer <manfred.steyer@gmx.net>
2019-06-04 13:13:32 +0200

    whatever

--- libs/shared/util-auth/src/lib/shared-util-auth.module.ts
+++ libs/shared/util-auth/src/lib/shared-util-auth.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { SharedUiAddressModule } from '@e-proc/shared/ui-address';
+import { SharedUiAddressModule } from '@e-proc/shared/ui-address';
 
 @NgModule({
   imports: [
     CommonModule,
-    // SharedUiAddressModule 
+    SharedUiAddressModule 
   ]
 })
 export class SharedUtilAuthModule {}
