8924c77b7b660a72bee975a399200cb48d9dbb4b
Manfred Steyer <manfred.steyer@gmx.net>
2019-10-20 09:25:46 +0200

    start

--- libs/catalog/feature-browse-products/src/lib/catalog-feature-browse-products.module.ts
+++ libs/catalog/feature-browse-products/src/lib/catalog-feature-browse-products.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
-import { CatalogDomainModule } from '@e-proc/catalog/domain';
 import { MatToolbarModule } from '@angular/material';
 import { MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule } from '@angular/material';
 import { ProductsComponent } from './products/products.component';
-
 import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { CatalogDomainModule } from '@e-proc/catalog/domain';
 
 @NgModule({
   imports: [
     CommonModule,
+
     CatalogDomainModule,
-    RouterModule.forChild([
-      /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
-    ]),
+
+    RouterModule.forChild([]),
     MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule
   ],
   declarations: [
